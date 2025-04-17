daf2eff9b6233c7aa8dd5d1977d06820e2ffb470
Kapilan <sriranjankapilan@gmail.com>
2025-04-17 19:32:58 +0530

    project setup created

---
+++ src/main/java/com/spring/chat/application/websocketwithdragonflydb/WebSocketWithDragonFlyDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.chat.application.websocketwithdragonflydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketWithDragonFlyDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketWithDragonFlyDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/chat/application/websocketwithdragonflydb/WebSocketWithDragonFlyDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.chat.application.websocketwithdragonflydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketWithDragonFlyDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
