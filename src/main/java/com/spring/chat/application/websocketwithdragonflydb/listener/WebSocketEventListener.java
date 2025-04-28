45114dcc60817f7e38730a3698f5b79df966b013
kapilan <k.sriranjan@homeit-system.de>
2025-04-28 16:38:59 +0530

    web socket event listener class aligned with necessary comments

--- src/main/java/com/spring/chat/application/websocketwithdragonflydb/listener/WebSocketEventListener.java
+++ src/main/java/com/spring/chat/application/websocketwithdragonflydb/listener/WebSocketEventListener.java
@@ -17,23 +17,32 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 public class WebSocketEventListener {
     private final RedisTemplate<String,Object> redisTemplate;
 
+    // Listens for WebSocket session connect events
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
         log.info("Received a new web socket connection");
     }
 
+    // Listens for WebSocket session disconnect events
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        // Access WebSocket session headers and attributes
         SimpMessageHeaderAccessor simpMessageHeaderAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+
+        // Retrieve the username stored in WebSocket session attributes during the connection
         String userName = (String) simpMessageHeaderAccessor.getSessionAttributes().get("username");
 
+        // If a user was identified in the session
         if(userName != null) {
+            // Create a ChatMessageDto to notify that the user has left because this function will execute when user left the session
             ChatMessageDto chatMessageDto = new ChatMessageDto();
             chatMessageDto.setMessage(userName+" has left the chat");
             chatMessageDto.setUserName(userName);
             chatMessageDto.setMessageType(MessageType.LEAVE);
             log.info("User disconnected from the chat "+userName);
-            redisTemplate.convertAndSend("chat",chatMessageDto);
+
+            // Publish the leave message to the Redis channel ("chat")
+             redisTemplate.convertAndSend("chat",chatMessageDto);
         }
     }
 }
