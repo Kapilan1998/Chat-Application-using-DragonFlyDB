706aaa696cdc91172a79e453751abdc6f2372be3
Kapilan <sriranjankapilan@gmail.com>
2025-11-15 19:59:46 +0530

    forces tests to use application-test.properties to deal Redis issue

--- src/test/java/com/spring/chat/application/websocketwithdragonflydb/WebSocketWithDragonFlyDbApplicationTests.java
+++ src/test/java/com/spring/chat/application/websocketwithdragonflydb/WebSocketWithDragonFlyDbApplicationTests.java
@@ -2,7 +2,12 @@ package com.spring.chat.application.websocketwithdragonflydb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+//During tests, do NOT use application.properties.
+//Instead load application-test.properties.
+@ActiveProfiles("test")
 @SpringBootTest
 class WebSocketWithDragonFlyDbApplicationTests {
 
